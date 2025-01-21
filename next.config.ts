e1a22269ce877565dd99fb967d11d4f5f0044893
Muhammad Haris Awan <111harisawan@gmail.com>
2025-01-21 19:36:11 +0500

    Updated configurations for image

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,17 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "cdn.sanity.io",
+        port: "", // Leave empty if no specific port is needed
+        pathname: "/images/**", // Matches all paths under /images/
+      },
+    ],
+  },
 };
 
 export default nextConfig;
